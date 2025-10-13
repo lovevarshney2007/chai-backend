edabf920b35628393dbb909601950b3761901408
varshneylove <lavvarshney2@gmail.com>
2025-10-13 22:14:45 +0530

    utill code discussion

---
+++ utils/AppError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        error = [],
+        statck = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.date = null,
+        this.message = message,
+        this.success = false
+        this.errors = errors
+
+        if(statck){
+            this.stack = stack
+
+        } else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
